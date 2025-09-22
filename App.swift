c66a82d54276f82745c3e2ca906591be6723de28
Sampath Bommakanti <sampath.in@gmail.com>
2025-09-22 21:21:13 +0200

    readded firebase

--- App.swift
+++ App.swift
@@ -1,17 +1,17 @@
 import SwiftUI
-// import FirebaseCore
+import FirebaseCore
 
 @main
 struct BookshelfScannerApp: App {
     init() {
         // Configure Firebase with your project details
-//         let options = FirebaseOptions(googleAppID: "1:244176711240:ios:0448a40563dbcd22bd727d",
-//                                     gcmSenderID: "244176711240")
-//         options.apiKey = "AIzaSyBLobo6IH1MTQY5n04PCqI-BvRL2ZMaWfE"
-//         options.projectID = "bookshelf-scanner-ios"
-//         options.storageBucket = "bookshelf-scanner-ios.firebasestorage.app"
+        let options = FirebaseOptions(googleAppID: "1:244176711240:ios:0448a40563dbcd22bd727d",
+                                    gcmSenderID: "244176711240")
+        options.apiKey = "AIzaSyBLobo6IH1MTQY5n04PCqI-BvRL2ZMaWfE"
+        options.projectID = "bookshelf-scanner-ios"
+        options.storageBucket = "bookshelf-scanner-ios.firebasestorage.app"
 
-//         FirebaseApp.configure(options: options)
+        FirebaseApp.configure(options: options)
     }
 
     var body: some Scene {
