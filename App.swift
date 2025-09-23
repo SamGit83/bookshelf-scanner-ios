59c61bd94a7fe8099f240e6b141c4d7881f07a0a
Sampath Bommakanti <sampath.in@gmail.com>
2025-09-23 19:23:29 +0200

    updates to splash screen.

--- App.swift
+++ App.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct BookshelfScannerApp: App {
+    init() {
+        // Configure Firebase using GoogleService-Info.plist
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,10 +1,8 @@
 import UIKit
-import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
-                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
 }
